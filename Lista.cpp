6556bf59214803a9360ac10d1cf463003fe58216
estalinbenitez <estalinbenitez@unitec.edu>
2014-05-15 12:52:38 -0600

    hhh

---
+++ Lista.cpp
@@ -0,0 +1,89 @@
+#include "Lista.h"
+
+Lista::Lista()
+{
+    inicio=NULL;
+
+
+
+}
+
+Lista::~Lista()
+{
+    //dtor
+}
+
+
+//Funcion que agrega ah la lista
+
+void Lista::agregar(Animal* animalito){
+
+    if(inicio!=NULL){
+            Animal*temp=inicio;
+            while(temp->sig!=NULL){
+                temp=temp->sig;
+            }
+                temp->sig=animalito;
+        }else{
+            inicio=animalito;
+        }
+
+
+}
+//Funcion Imprimir
+void Lista::imprimir(){
+
+    Animal*tmp=inicio;
+    while(tmp!=NULL){
+        cout<<tmp->tipo<<" "<<tmp->edad<<" "<<tmp->nombre<<" "<<endl;
+        tmp=tmp->sig;
+    }
+}
+
+void Lista::escribir(){
+
+    write.open("Listas.txt");
+
+    cout<<"Escribiendo Datos en Archivo"<<endl;
+    Animal*tmp=inicio;
+     while(tmp!=NULL){
+    write<<tmp->tipo<<" "<<tmp->edad<<" "<<tmp->nombre<<" "<<endl;
+
+        tmp=tmp->sig;
+    }
+
+    write.close();
+}
+
+void Lista::leer(){
+    read.open("Listas.txt");
+
+    cout<<"Leyendo Lista Del Archivo"<<endl;
+    string str;
+    string edad;
+    string nombre;
+
+    read.seekg(0);
+
+    while(read>>str>>edad>>nombre){
+
+        int e=atoi(edad.c_str());
+
+
+        if(str=="Perro")
+            agregar(new Perro(e,nombre));
+        if(str=="Canario")
+            agregar(new Canario(e,nombre));
+        if(str=="Lora")
+            agregar(new Lora(e,nombre));
+
+
+    }
+
+
+    read.close();
+}
+
+
+
+
---
+++ Lista.h
@@ -0,0 +1,29 @@
+#ifndef LISTA_H
+#define LISTA_H
+#include "Animal.h"
+#include "Perro.h"
+#include "Lora.h"
+#include "Canario.h"
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+class Lista
+{
+    public:
+        Animal*inicio;
+        ofstream write;
+        ifstream read;
+        void agregar(Animal*animalito);
+        void imprimir();
+        void escribir();
+        void leer();
+
+        Lista();
+        virtual ~Lista();
+    protected:
+    private:
+};
+
+#endif // LISTA_H
